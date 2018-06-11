ff21a74c3de977e46439a1f7c8a61ec99c866a74
Gregory McCubbin <gwmccubbin@gmail.com>
2018-06-11 15:07:52 -0500

    SPIKE - attempts to add  and  headers to optionsi

--- lib/faraday/options.rb
+++ lib/faraday/options.rb
@@ -190,7 +190,7 @@ module Faraday
 
   class RequestOptions < Options.new(:params_encoder, :proxy, :bind,
     :timeout, :open_timeout, :boundary,
-    :oauth)
+    :oauth, :etag, :async)
 
     def []=(key, value)
       if key && key.to_sym == :proxy
